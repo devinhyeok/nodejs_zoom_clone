e25d7eb67416690e56b16ceabd2a7fef0b623799
devinhyeok <94621970+devinhyeok@users.noreply.github.com>
2022-11-21 18:13:05 +0900

    ch2_7_nicknames

--- src/server.js
+++ src/server.js
@@ -17,21 +17,23 @@ const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
 wsServer.on("connection", (socket) => {
+    socket["nickname"] = "anonymous";
     socket.onAny((event) => {
         console.log(`Socket Event: ${event}`);
     });
     socket.on("enter_room", (roomName, done) => {
         socket.join(roomName);
         done();
-        socket.to(roomName).emit("welcome"); // 방에 있는 모두에게 브로드캐스트
+        socket.to(roomName).emit("welcome", socket.nickname); // 방에 있는 모두에게 브로드캐스트
     });
     socket.on("disconnecting", () => {
-        socket.rooms.forEach(room => socket.to(room).emit("bye"));
+        socket.rooms.forEach(room => socket.to(room).emit("bye", socket.nickname));
     });
     socket.on("new_message", (msg, room, done) => {
-        socket.to(room).emit("new_message", msg);
+        socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
         done();
     });
+    socket.on("nickname", (nickname) => (socket["nickname"] = nickname));
 });
 
 httpServer.listen(3000, handleListen);
